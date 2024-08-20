782290a2616c848ea805a7f8bf74fc0ea930a3c4
Jiyesss <122000486+Jiyesss@users.noreply.github.com>
2024-08-20 16:16:41 +0900

    5: 싱글톤 패턴

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SingletonTest {
 
     @Test
@@ -25,7 +27,21 @@ public class SingletonTest {
         System.out.println("memberService2: " + memberService2);
 
         // memberservice1 != memberservice2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+
+
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1: " + singletonService1);
+        System.out.println("singletonService2: " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
 
 
     }
